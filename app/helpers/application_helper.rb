30c1ee85f3dbadfc98ba6d00595fbf54981af3fd
Polina Soshnin <psoshnin@wellesley.edu>
2015-10-23 22:12:42 -0700

    create alternative sign in box

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+	#allow login anywhere in the app
+	def resource_name
+    	:user
+  	end
+
+  	def resource
+    	@resource ||= User.new
+  	end
+
+  	def devise_mapping
+    	@devise_mapping ||= Devise.mappings[:user]
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+
+  devise_for :users 
+
+  devise_scope :user do
+    get 'login' => 'devise/sessions#new'
+    post 'login' => 'devise/sessions#create'
+  end
+
   resources :entries
+  resources :topics
   get 'about' => 'welcome#about'
 
   get 'index' => 'entries#index'
