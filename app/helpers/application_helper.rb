f458fbce8578b1f1e7636ccfb7f21ef7da43782d
Polina Soshnin <psoshnin@wellesley.edu>
2015-11-24 16:39:08 -0800

    refactor entry views into partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,14 +2,22 @@ module ApplicationHelper
 
 	#allow login anywhere in the app
 	def resource_name
-    	:user
-  	end
+    :user
+  end
 
-  	def resource
-    	@resource ||= User.new
-  	end
+	def resource
+  	@resource ||= User.new
+	end
 
-  	def devise_mapping
-    	@devise_mapping ||= Devise.mappings[:user]
-  	end
+	def devise_mapping
+  	@devise_mapping ||= Devise.mappings[:user]
+	end
+
+  def form_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form-group has-error'
+    else
+      content_tag :div, capture(&block), class: 'form-group'
+    end
+  end
 end
